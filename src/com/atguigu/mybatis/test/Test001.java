5713237296386a4d170dac3787c9dd1ff7874fe4
menjinqiu <542957431@qq.com>
2016-03-19 01:06:14 +0800

    chongtu 2

--- src/com/atguigu/mybatis/test/Test001.java
+++ src/com/atguigu/mybatis/test/Test001.java
@@ -129,6 +129,7 @@ public class Test001 {
 			PersonMapper mapper = sqlSession.getMapper(PersonMapper.class);
 			List<Person> list = mapper.getAll();
 			for (Person person : list) {
+				System.out.println("hello github");
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
